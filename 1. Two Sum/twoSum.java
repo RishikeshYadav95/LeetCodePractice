3f12daef01ce37f238c3203ab40f8d62f976be7d
RishikeshYadav95 <rishikesh21095@gmail.com>
2024-01-13 01:16:47 -0500

    Updated code for two Sum

--- 1. Two Sum/twoSum.java
+++ 1. Two Sum/twoSum.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 public class twoSum {
     public int[] twoSum(int[] nums, int target) {
-        HashMap vault = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> vault = new HashMap<Integer, Integer>();
         for(int i = 0; i < nums.length; i++){
             if(vault.containsKey(nums[i])) {
             	return new int[] {(int)vault.get(nums[i]), i};         	
